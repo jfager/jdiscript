732d93c3a4ec03a8f817342dc8e513f3006dc7e5
Jason Fager <jfager@gmail.com>
2010-07-28 21:10:27 -0400

    make HelloWorld a little nicer to play with

--- src/main/java/org/jdiscript/example/HelloWorld.java
+++ src/main/java/org/jdiscript/example/HelloWorld.java
@@ -1,28 +1,19 @@
 package org.jdiscript.example;
 
-import java.util.HashMap;
-import java.util.Map;
-
 //A test class you can use for simple debugging scripts.
 public class HelloWorld {
 	private String helloTo;
-	private final Map<String, String> map;
 
 	public HelloWorld() {
 		this("World");
 	}
 	
 	public HelloWorld(String helloTo) {
-		if(helloTo != null) {
-			this.helloTo = new String("to " + helloTo);
-		}
-		this.map = new HashMap<String, String>();
-		map.put("Hello", helloTo);
+		this.helloTo = helloTo;
 	}
 	
 	public String sayHello() {
-		String to = map.get("Hello");
-		return "Hello, " + helloTo + ", and " + to +"!";
+		return "Hello, " + helloTo;
 	}
 	
 	public static void main(String[] args) {
@@ -32,6 +23,8 @@ public class HelloWorld {
 		System.out.println(hello.sayHello());
 		
 		hello = new HelloWorld("Barry");
+		System.out.println(hello.sayHello());
+		
 		System.out.println(hello.sayHello());
 	}
 }
