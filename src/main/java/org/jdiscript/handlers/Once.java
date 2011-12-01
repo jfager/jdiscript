158838c1491f65171260cda384abfb5e8b519a63
Jason Fager <jfager@gmail.com>
2011-11-30 21:29:47 -0500

    add Once class

---
+++ src/main/java/org/jdiscript/handlers/Once.java
@@ -0,0 +1,17 @@
+package org.jdiscript.handlers;
+
+import org.jdiscript.events.DebugEventDispatcher;
+
+import com.sun.jdi.event.Event;
+
+public class Once extends BaseEventHandler {
+	private DebugEventHandler handler;
+	public Once(DebugEventHandler handler) {
+		this.handler = handler;
+	}
+	@Override
+    public void unhandledEvent( Event e ) {
+		DebugEventDispatcher.doFullDispatch(e, handler);
+		e.request().disable();
+    }
+}
